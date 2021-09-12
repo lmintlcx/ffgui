8ffc5a88a12018bf31d2f16a732f83e0d463a482
lmintlcx <lmintlcx@gmail.com>
2021-09-12 16:05:38 +0800

    20210912

--- build.bat
+++ build.bat
@@ -8,10 +8,10 @@ cd /d D:\tmp\ffgui_build
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86 10.0.17763.0
 
-C:\Qt\Qt_5.15.1\msvc2017_x86\static\bin\qmake.exe D:\work\ffgui\ffgui.pro -spec win32-msvc
+C:\Dev\Qt_5.15.2\msvc2017_x86\static\bin\qmake.exe D:\work\ffgui\ffgui.pro -spec win32-msvc
 
-REM C:\Qt\Qt_5.15.1\msvc2017_x86\static\bin\lupdate.exe D:\work\ffgui\ffgui.pro
-REM C:\Qt\Qt_5.15.1\msvc2017_x86\static\bin\lrelease.exe D:\work\ffgui\ffgui.pro
+REM C:\Dev\Qt_5.15.2\msvc2017_x86\static\bin\lupdate.exe D:\work\ffgui\ffgui.pro
+REM C:\Dev\Qt_5.15.2\msvc2017_x86\static\bin\lrelease.exe D:\work\ffgui\ffgui.pro
 
 del D:\tmp\ffgui_build\debug\ffgui.exe
 del D:\tmp\ffgui_build\release\ffgui.exe
